87ff501213127ec2ca23436d017c913e2e1653e7
Karzaandco <karzaandco@gmail.com>
2025-03-07 15:53:11 -0600

    Initial Commit

---
+++ Assignment3_CardGameUITests/Assignment3_CardGameUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Assignment3_CardGameUITestsLaunchTests.swift
+//  Assignment3_CardGameUITests
+//
+//  Created by Karter Caves on 3/7/25.
+//
+
+import XCTest
+
+final class Assignment3_CardGameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
